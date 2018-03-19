673c0be21be62b8a3ffef46662246642c2e41d22
fadika06 <fadikariansya06@gmail.com>
2018-03-19 19:30:13 +0700

    update controller and others

--- src/Models/Bantenprov/Anggaran/Anggaran.php
+++ src/Models/Bantenprov/Anggaran/Anggaran.php
@@ -17,6 +17,7 @@ class Anggaran extends Model
     ];
     protected $fillable = [
         'group_egovernment_id',
+        'sector_egovernment_id',
         'user_id',
         'label',
         'description'
@@ -25,6 +26,11 @@ class Anggaran extends Model
     public function group_egovernment()
     {
         return $this->belongsTo('Bantenprov\GroupEgovernment\Models\Bantenprov\GroupEgovernment\GroupEgovernment','group_egovernment_id');
+    }
+
+    public function sector_egovernment()
+    {
+        return $this->belongsTo('Bantenprov\SectorEgovernment\Models\Bantenprov\SectorEgovernment\SectorEgovernment','sector_egovernment_id');
     }
 
     public function user()
